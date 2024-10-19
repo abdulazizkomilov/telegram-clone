f2dc5ebae05041f78263ac2e9c828cada1c5c575
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-10-19 06:28:03 +0500

    CI/CD changed

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
 SECRET_KEY = "django-insecure-h+hioiqdj&dwja1h$@12=s8ra9zw%$-$!9txds$bh^ask+0iij"
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
