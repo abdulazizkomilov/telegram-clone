e1c83f8454c0fcb65e16cdb3ec88293f77c686e4
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-11-06 15:12:43 +0500

    channel message schedule unit test

---
+++ apps/tests/channel/channel_message_schedule/test_message_schedule.py
@@ -0,0 +1,73 @@
+import pytest
+from rest_framework import status
+from django.utils import timezone
+from unittest.mock import MagicMock
+from share.services import TokenService
+from channel.models import Channel, ChannelScheduledMessage
+
+
+@pytest.mark.django_db
+class TestCreateScheduledMessageView:
+    @pytest.fixture
+    def setup_data(self, user_factory):
+        owner = user_factory.create()
+        other_user = user_factory.create()
+        channel = Channel.objects.create(name="Test Channel", owner=owner)
+        return {
+            "owner": owner,
+            "other_user": other_user,
+            "channel": channel
+        }
+
+    def test_successful_message_scheduling(self, setup_data, api_client, tokens, mocker):
+        mock_redis_client = MagicMock()
+        mocker.patch.object(TokenService, 'get_redis_client', return_value=mock_redis_client)
+
+        access, _ = tokens(setup_data["owner"])
+        client = api_client(access)
+
+        mock_redis_client.smembers.return_value = {access.encode()}
+
+        data = {
+            "text": "Scheduled message",
+            "scheduled_time": (timezone.now() + timezone.timedelta(minutes=10)).isoformat()
+        }
+
+        response = client.post(f"/api/channels/{setup_data['channel'].id}/messages/schedule/", data, format="json")
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert ChannelScheduledMessage.objects.filter(channel=setup_data["channel"]).exists()
+
+    def test_channel_not_found(self, setup_data, mocker, api_client, tokens):
+        mock_redis_client = MagicMock()
+        mocker.patch.object(TokenService, 'get_redis_client', return_value=mock_redis_client)
+
+        access, _ = tokens(setup_data["owner"])
+        client = api_client(access)
+
+        mock_redis_client.smembers.return_value = {access.encode()}
+        data = {
+            "text": "Message for non-existent channel",
+            "scheduled_time": (timezone.now() + timezone.timedelta(minutes=10)).isoformat()
+        }
+
+        response = client.post(f"/api/channels/1/messages/schedule/", data, format="json")
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_scheduled_time_in_the_past(self, setup_data, mocker, api_client, tokens):
+        mock_redis_client = MagicMock()
+        mocker.patch.object(TokenService, 'get_redis_client', return_value=mock_redis_client)
+
+        access, _ = tokens(setup_data["owner"])
+        client = api_client(access)
+
+        mock_redis_client.smembers.return_value = {access.encode()}
+        data = {
+            "text": "Past message",
+            "scheduled_time": (timezone.now() - timezone.timedelta(minutes=10)).isoformat()
+        }
+
+        response = client.post(f"/api/channels/{setup_data['channel'].id}/messages/schedule/", data, format="json")
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
---
+++ apps/tests/channel/channel_message_schedule/test_scheduled_message_task.py
@@ -0,0 +1,57 @@
+import pytest
+from unittest.mock import patch
+from django.utils import timezone
+from channel.models import ChannelScheduledMessage, ChannelMessage, Channel
+from channel.tasks import send_channel_scheduled_message
+from user.models import NotificationPreference
+
+
+@pytest.mark.django_db
+class TestSendChannelScheduledMessageTask:
+    @pytest.fixture
+    def setup_data(self, user_factory):
+        owner = user_factory.create()
+        channel = Channel.objects.create(name="Test Channel", owner=owner)
+
+        member_with_notifications = user_factory.create()
+        notification_preference = NotificationPreference.objects.create(
+            user=member_with_notifications,
+            notifications_enabled=True,
+            device_token="token1"
+        )
+        member_with_notifications.notification_preference = notification_preference
+        member_with_notifications.save()
+
+        member_without_notifications = user_factory.create()
+        channel.memberships.create(user=member_with_notifications)
+        channel.memberships.create(user=member_without_notifications)
+
+        # Create a scheduled message
+        scheduled_message = ChannelScheduledMessage.objects.create(
+            channel=channel,
+            sender=owner,
+            text="Scheduled message",
+            scheduled_time=timezone.now() - timezone.timedelta(minutes=5),
+            sent=False
+        )
+        return {
+            "channel": channel,
+            "scheduled_message": scheduled_message,
+            "member_with_notifications": member_with_notifications,
+            "member_without_notifications": member_without_notifications
+        }
+
+    @patch("channel.tasks.send_push_notification.delay")
+    def test_send_scheduled_messages(self, mock_send_push_notification, setup_data):
+        send_channel_scheduled_message()
+
+        scheduled_message = ChannelScheduledMessage.objects.get(id=setup_data["scheduled_message"].id)
+        assert scheduled_message.sent is True
+
+        assert ChannelMessage.objects.filter(channel=setup_data["channel"]).count() == 1
+
+        mock_send_push_notification.assert_called_once_with(
+            setup_data["member_with_notifications"].notification_preference.device_token,
+            f"New Message in {setup_data['channel'].name}",
+            setup_data["scheduled_message"].text
+        )
