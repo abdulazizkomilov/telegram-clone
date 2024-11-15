1e33422b75b28078b1670a29d5d9acef97ec3302
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-11-15 14:44:40 +0500

    boolbar

--- core/urls.py
+++ core/urls.py
@@ -41,8 +41,9 @@ urlpatterns = [
             ]
         ),
     ),
-] + debug_toolbar_urls()
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += debug_toolbar_urls
